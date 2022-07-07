f6c350757d72bce1517c61f80f78682537676c4c
KuromiSoma <tamagoyaki.zyukuzyuku@gmail.com>
2022-07-07 17:57:25 +0900

    UketukeService編集

--- src/main/java/com/example/demo/UketukeService.java
+++ src/main/java/com/example/demo/UketukeService.java
@@ -7,6 +7,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 @Service
 @Transactional 
 public class UketukeService {
@@ -18,5 +21,10 @@ public class UketukeService {
 		List<Uketuke> uketukeList = jdbcTemplate.queryForObject("select * from java_user.uketuke", new UketukeMapper());
 
 		return uketukeList;
+	}
+	
+	public boolean insertUketuke(Model model, @ModelAttribute  UketukeForm uketukeForm) {
+		jdbcTemplate.update("INSERT INTO java_user.uketuke(sei_name,mei_name,nk_add,nm_add1,nm_add2,nm_add3,nk_tel,gender_no,birthday_dt,nk_mail,nk_pass) VALUES(?,?,?,?,?,?,?,?,to_timestamp(?,'yyyy-MM-dd'),?,?)",uketukeForm.getSeiName(),uketukeForm.getMeiName(),uketukeForm.getNkAdd(),uketukeForm.getNmAdd1(),uketukeForm.getNmAdd2(),uketukeForm.getNmAdd3(),uketukeForm.getNkTel(),uketukeForm.getGenderNo(),uketukeForm.getBirthdayDt(),uketukeForm.getNkMail(),uketukeForm.getNkPass());
+		return true;
 	}
 }
